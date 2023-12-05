c6d9b53be98b4b5f3f43d10b99e10d5305660e63
Luka <lukadbarth@hotmail.com>
2023-12-05 06:10:05 -0300

    mend
    This challenge consists of calculating the BMI

--- Desafios/ex002.php
+++ Desafios/ex002.php
@@ -9,4 +9,12 @@ $imc = ($peso / ($altura ** 2));
 
 $imc = number_format($imc, 2, '.', '');
 
-echo "O seu IMC é de $imc.";
+echo "O seu IMC é de $imc. Você está com o IMC ";
+
+if ($imc < 18) {
+  echo "abaixo do recomendado.";
+} else if ($imc < 25) {
+  echo "recomendado.";
+} else {
+  echo "acima do recomendado.";
+}
