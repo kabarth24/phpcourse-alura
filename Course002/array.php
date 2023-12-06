111471ece5b8cac69e795f5733b0a8f0eb326bc0
Luka <lukadbarth@hotmail.com>
2023-12-06 06:09:43 -0300

    I learned about arrays in PHP. I learned that they can store as many items as I want, the syntax, how to access the elements within the list, that arrays start at index 0 and a function to know how many items are in a given array

---
+++ Course002/array.php
@@ -0,0 +1,7 @@
+<?php
+
+$ageList = [23, 25, 27, 30, 21, 19, 28];
+
+$age = $ageList[0];
+
+echo "$age";
---
+++ Course002/loop-array.php
@@ -0,0 +1,7 @@
+<?php
+
+$ageList = [23, 25, 27, 30, 21, 19, 28];
+
+for ($i = 0; $i < count($ageList); $i++) {
+  echo "$ageList[$i]" . PHP_EOL;
+}
