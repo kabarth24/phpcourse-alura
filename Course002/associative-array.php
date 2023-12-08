3db86aa4cd4c01376e10489aa40ce35ad957f07b
Luka <lukadbarth@hotmail.com>
2023-12-08 05:40:16 -0300

    I learned about associative arrays. I learned the difference between numeric and associative arrays and how to reference these arrays

---
+++ Course002/associative-array.php
@@ -0,0 +1,18 @@
+<?php
+
+$conta1 = [
+  "titular" => "Luka",
+  "saldo" => "1000",
+];
+
+$conta2 = [
+  "titular" => "Maria",
+  "saldo" => "10000",
+];
+
+$conta3 = [
+  "titular" => "Alberto",
+  "saldo" => "300",
+];
+
+$contasCorrentes = [$conta1, $conta2, $conta3];
